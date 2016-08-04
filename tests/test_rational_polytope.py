6a99f8e7661d0ce76558f56046750b1340157433
Vincent Delecroix <vincent.delecroix@labri.fr>
2016-08-04 14:59:15 -0400

    more test for rational polytope

--- tests/test_rational_polytope.py
+++ tests/test_rational_polytope.py
@@ -14,10 +14,86 @@ p2 = polymake.Polytope('POINTS', m2)
 p3 = polymake.Polytope('POINTS', m3)
 
 class TestPolymakePolytope(unittest.TestCase):
+    def test_AFFINE_HULL(self):
+        a3 = p3.AFFINE_HULL
+        self.assertEqual(a3[0,0], -3)
+        self.assertEqual(a3[0,1], 1)
+        self.assertEqual(a3[0,2], 1)
+        self.assertEqual(a3[0,3], 1)
+
     def test_ALTSHULER_DET(self):
         self.assertEqual(p1.ALTSHULER_DET, 0)
         self.assertEqual(p2.ALTSHULER_DET, 1872)
         self.assertEqual(p3.ALTSHULER_DET, 4)
+
+    def test_BALANCE(self):
+        self.assertEqual(p1.BALANCE, 1)
+        self.assertEqual(p2.BALANCE, 1)
+        self.assertEqual(p3.BALANCE, 2)
+
+    def test_BALANCED(self):
+        self.assertTrue(p1.BALANCED)
+        self.assertTrue(p2.BALANCED)
+        self.assertTrue(p3.BALANCED)
+
+    def test_BOUNDED(self):
+        self.assertTrue(p1.BOUNDED)
+        self.assertTrue(p2.BOUNDED)
+        self.assertTrue(p3.BOUNDED)
+
+    def test_CENTERED(self):
+        self.assertFalse(p1.CENTERED)
+        self.assertFalse(p2.CENTERED)
+        self.assertFalse(p3.CENTERED)
+
+    def test_CENTRALLY_SYMMETRIC(self):
+        self.assertFalse(p1.CENTRALLY_SYMMETRIC)
+        self.assertFalse(p2.CENTRALLY_SYMMETRIC)
+        self.assertFalse(p3.CENTRALLY_SYMMETRIC)
+
+    def test_COCUBICAL(self):
+        self.assertFalse(p1.COCUBICAL)
+        self.assertFalse(p2.COCUBICAL)
+        self.assertTrue(p3.COCUBICAL)
+
+    def test_COMBINATORIAL_DIM(self):
+        self.assertEqual(p1.COMBINATORIAL_DIM, 3)
+        self.assertEqual(p2.COMBINATORIAL_DIM, 3)
+        self.assertEqual(p3.COMBINATORIAL_DIM, 2)
+
+    def test_CUBICAL(self):
+        self.assertTrue(p1.CUBICAL)
+        self.assertFalse(p2.CUBICAL)
+        self.assertTrue(p3.CUBICAL)
+
+    def test_CUBICAL_H_VECTOR(self):
+        v = p1.CUBICAL_H_VECTOR
+        self.assertEqual(v[0], 4)
+        self.assertEqual(v[1], 4)
+        self.assertEqual(v[2], 4)
+        self.assertEqual(v[3], 4)
+
+        v = p3.CUBICAL_H_VECTOR
+        self.assertEqual(v[0], 2)
+        self.assertEqual(v[1], 1)
+        self.assertEqual(v[2], 2)
+
+    def test_CUBICALITY(self):
+        self.assertEqual(p1.CUBICALITY, 3)
+        self.assertEqual(p2.CUBICALITY, 1)
+        self.assertEqual(p3.CUBICALITY, 1)
+
+    def test_DUAL_BOUNDED_H_VECTOR(self):
+        v = p1.DUAL_BOUNDED_H_VECTOR
+        self.assertEqual(v[0], 1)
+        self.assertEqual(v[1], 3)
+        self.assertEqual(v[2], 3)
+        self.assertEqual(v[3], 1)
+
+        v = p3.DUAL_BOUNDED_H_VECTOR
+        self.assertEqual(v[0], 1)
+        self.assertEqual(v[1], 1)
+        self.assertEqual(v[2], 1)
 
     def test_N_FACETS(self):
         self.assertEqual(p1.N_FACETS, 6)
